ae129246df1edf8ab91a25bceece5f8185d575e2
hector <polbarazorda@gmail.com>
2021-10-11 00:05:00 -0500

    fix router

--- src/components/Shows/Item.jsx
+++ src/components/Shows/Item.jsx
@@ -8,7 +8,13 @@ import { Title } from '../Base/Title'
 export const Item = ({ item, tvShow = false }) => {
   return (
     <Container>
-      <Link href={tvShow ? `/tvshow/${item.id}` : `/detail/${item.id}`}>
+      <Link
+        href={
+          tvShow
+            ? `/tvshow/${encodeURIComponent(item.id)}`
+            : `/detail/${encodeURIComponent(item.id)}`
+        }
+      >
         <a>
           <Img src={`${urls[0]?.images + item.poster_path}`} />
           <Info>
